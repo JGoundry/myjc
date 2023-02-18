83ba6fe41edea1099a6daa73c17aeaa565de7b92
Josh Goundry <joshgoundry@Joshs-MacBook-Pro.local>
2023-02-18 01:40:38 +0000

    Lexer partially implemented

--- Lexer Files/lexer.c
+++ Lexer Files/lexer.c
@@ -14,20 +14,26 @@ Email: sc21j2lg@leeds.ac.uk
 Date Work Commenced:
 *************************************************************************/
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include "lexer.h"
 
-
 // YOU CAN ADD YOUR OWN FUNCTIONS, DECLARATIONS AND VARIABLES HERE
 
+// File pointer and character iterator
+FILE *f;
+int c;
 
+const char *ReservedWords[] = {"class", "constructor", "method", "function:",
+							   "int", "boolean", "char", "void:",
+							   "var", "static", "field:",
+							   "let", "do", "if", "else", "while", "return:",
+							   "true", "false", "null:", "this:"};
 
-
-
+const char *Symbols[] = {"(", ")", "[", "]", "{", "}", ",", ";", "=", ".",
+						 "+", "-", "*", "/", "&", "|", "~", "<", ">"};
 
 // IMPLEMENT THE FOLLOWING functions
 //***********************************
@@ -37,42 +43,149 @@ Date Work Commenced:
 // This requires opening the file and making any necessary initialisations of the lexer
 // If an error occurs, the function should return 0
 // if everything goes well the function should return 1
-int InitLexer (char* file_name)
+int InitLexer(char *file_name)
 {
-  return 0;
+	f = fopen(file_name, "r");
+	if (f == 0)
+	{ // failure
+		return 0;
+	}
+
+	return 1;
 }
 
-
 // Get the next token from the source file
-Token GetNextToken ()
+Token GetNextToken()
 {
 	Token t;
-  t.tp = ERR;
-  return t;
+	t.tp = ERR;
+	c = getc(f);
+
+	// Remove whitespace
+	while (isspace(c))
+	{
+		c = getc(f);
+	}
+
+	// Check for comment, remember EOF comment
+	if (c == '/')
+	{
+		// Save current pos
+		fpos_t *pos;
+		fgetpos(f, pos);
+
+		c = getc(f);
+
+		// Comment to end of line
+		if (c == '/')
+		{
+			while (c != '\n' && c != EOF)
+			{
+				c = getc(f);
+			}
+			if (c == EOF)
+			{
+				t.tp = ERR;
+				t.ec = EofInStr;
+				return t;
+			}
+			else
+			{
+				// Skip newline
+				c = getc(f);
+			}
+		}
+
+		// Comment until closing
+		else if (c == '*')
+		{
+			c = getc(f);
+
+			if (c == '*')
+			{
+				// API documentation comment
+				c = getc(f);
+
+			}
+			while (1)
+			{
+				if (c == '*')
+				{
+					c = getc(f);
+					if (c == '/')
+						break;
+				}
+				else if (c == EOF)
+				{
+					t.tp = ERR;
+					t.ec = EofInStr;
+					return t;
+				}
+			}
+		}
+		else
+		{
+			// Not a comment, move iterator back
+			c = '/';
+			fsetpos(f, pos);
+		}
+	}
+
+	// Check for EOF
+	if (c == EOF) {
+		t.tp = EOFile;
+		return t;
+	}
+
+	// Temp lexeme storage
+    char lexeme[128];
+
+	// Check for string (surrounded with double quotes) EofInStr & NewLnInStr, rememember string lex error codes
+	if (c == '"'){
+
+	}
+
+	// Check for reserved words and id (identifier must start with _ or letter)
+	if (isalpha(c) || c == '_') {
+
+	}
+
+	// Check for symbol
+
+	// Else must be illegal symbol
+
+	// Check for
+	return t;
 }
 
 // peek (look) at the next token in the source file without removing it from the stream
-Token PeekNextToken ()
+Token PeekNextToken()
 {
-  Token t;
-  t.tp = ERR;
-  return t;
+	Token t;
+	t.tp = ERR;
+
+	// Peek current pos in file
+
+	// Call GetNextToken
+
+	// Reset to old pos in file
+
+	return t;
 }
 
 // clean out at end, e.g. close files, free memory, ... etc
-int StopLexer ()
+int StopLexer()
 {
 	return 0;
 }
 
 // do not remove the next line
 #ifndef TEST
-int main ()
+int main()
 {
 	// implement your main function here
-  // NOTE: the autograder will not use your main function
+	// NOTE: the autograder will not use your main function
 
-  
 	return 0;
 }
 // do not remove the next line
