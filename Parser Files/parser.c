89a3ab7021258fdd380d5bc5d4eeb2de0c5ed478
Josh Goundry <joshuagoundry@gmail.com>
2023-03-08 22:59:24 +0000

    Added lexer to parser files and created some comments

--- Parser Files/parser.c
+++ Parser Files/parser.c
@@ -8,11 +8,35 @@
 
 // you can declare prototypes of parser functions below
 
+// each grammar rule is a function
 
+// classDeclar
+// memberDeclar
+// classVarDeclar
+// type
+// subroutineDeclar
+// paramList
+// subroutineBody
+// statement
+// varDeclarStatement
+// letStatement
+// ifStatement
+// whileStatement
+// doStatement
+// subroutineCall
+// expressionList
+// returnStatement
+// expression
+// relationalExpression
+// ArithmeticExpression
+// term
+// factor
+// operand
 
 
 int InitParser (char* file_name)
 {
+	InitLexer(file_name);
 
 	return 1;
 }
@@ -23,6 +47,11 @@ ParserInfo Parse ()
 
 	// implement the function
 
+	Token t;
+	t = GetNextToken();
+
+	printf("Token: %s\n", t.lx);
+
 
 	pi.er = none;
 	return pi;
@@ -31,12 +60,16 @@ ParserInfo Parse ()
 
 int StopParser ()
 {
+	StopLexer();
 	return 1;
 }
 
 #ifndef TEST_PARSER
 int main ()
 {
+	InitParser("Ball.jack");
+	Parse();
+	StopParser();
 
 	return 1;
 }
